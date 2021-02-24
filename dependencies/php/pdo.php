ff54011909620180d105dec567fd76f3c80c8e91
Tim Mulch <42800616+TimMulch@users.noreply.github.com>
2021-02-24 16:12:40 +0100

    Update pdo.php

--- dependencies/php/pdo.php
+++ dependencies/php/pdo.php
@@ -1,6 +1,6 @@
 <?php
-$dbservername = "REVOKED";
-$dbusername = "REVOKED";
-$dbpassword = "REVOKED"; // Sensitive data, do not commit to Github
-$connection = new PDO("mysql:host=$dbservername;dbname=REVOKED", $dbusername, $dbpassword);
+$dbservername = "REDACTED";
+$dbusername = "REDACTED";
+$dbpassword = "REDACTED"; // Sensitive data, do not commit to Github
+$connection = new PDO("mysql:host=$dbservername;dbname=REDACTED", $dbusername, $dbpassword);
 ?>
