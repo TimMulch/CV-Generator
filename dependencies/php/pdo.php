5d2cc88d700e4828d3ba6f3254e6177c261e9572
Tim Mulch <42800616+TimMulch@users.noreply.github.com>
2021-02-24 16:11:52 +0100

    Update pdo.php

--- dependencies/php/pdo.php
+++ dependencies/php/pdo.php
@@ -1,5 +1,5 @@
 <?php
-$dbservername = "daanklein.nl:3310";
+$dbservername = "sike nigga";
 $dbusername = "root";
 $dbpassword = "tim_hackt_nasa_met_html"; // Sensitive data, do not commit to Github
 $connection = new PDO("mysql:host=$dbservername;dbname=cv_generator", $dbusername, $dbpassword);
